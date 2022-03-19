aa79be68d9afb281d0e28a12fb4cb8759b4ec9d1
ameen <ameen@DESKTOP-HFIM49E>
2022-03-19 11:05:48 +0530

    pass gecko driver

--- root/src/test/java/root/root/DemoShoptest.java
+++ root/src/test/java/root/root/DemoShoptest.java
@@ -1,15 +1,22 @@
 package root.root;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DemoShoptest {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
 		driver.get("http://demowebshop.tricentis.com/");
+		driver.manage().window().maximize(); 
+		
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 		driver.findElement(By.partialLinkText("Log")).click();
 		driver.close();
 	}
-}
+} 
+	
